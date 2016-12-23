ac602a56baf6fe77f87f8329bcc27cb74ec1b114
Vyacheslav Kruglov <v.kruglov69@gmail.com>
2016-12-23 14:39:08 +0200

    add spring and spring-security dependencies into gradle, create SpringWebConfiguration

---
+++ src/main/java/com/ss/schedule/configuration/SpringWebConfiguration.java
@@ -0,0 +1,34 @@
+package com.ss.schedule.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Created by vyach on 23.12.2016.
+ */
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({"com.ss.schedule.web"})
+public class SpringWebConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("/");
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
