10dd8cb14e940c85ac8d7dbeb62f8e0ab380b44c
starovoit777 <starovoit777@gmail.com>
2017-01-05 10:35:42 +0200

    Add import for configureDefaultServletHandling

--- src/main/java/ua/cv/tim/configuration/WebConfiguration.java
+++ src/main/java/ua/cv/tim/configuration/WebConfiguration.java
@@ -3,11 +3,11 @@ package ua.cv.tim.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Created by vyach on 28.12.2016.
