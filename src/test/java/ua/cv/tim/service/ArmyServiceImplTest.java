b15137c14bc25d1e06a4d5bb17e3188e85525442
starovoit777 <starovoit777@gmail.com>
2017-01-13 14:59:40 +0200

    working comit

--- src/test/java/ua/cv/tim/service/ArmyServiceImplTest.java
+++ src/test/java/ua/cv/tim/service/ArmyServiceImplTest.java
@@ -1,24 +1,48 @@
 package ua.cv.tim.service;
 
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.Test;
 import ua.cv.tim.configuration.HibernateConfiguration;
-
-import static org.junit.Assert.*;
+import ua.cv.tim.model.Army;
 
 /**
  * Created by okunetc on 12.01.2017.
  */
 @WebAppConfiguration
 @ContextConfiguration(classes = {HibernateConfiguration.class})
-public class ArmyServiceImplTest {
+public class ArmyServiceImplTest extends AbstractTestNGSpringContextTests {
+
     @Autowired
-    UserService userService;
+    ArmyService armyService;
+
+
+    @Test
+    public void getArmyById() {
+        armyService.getArmyById("4046c02c-24e5-486b-8127-2848da877ba6");
+        System.out.print(armyService.getArmyById("4046c02c-24e5-486b-8127-2848da877ba6"));
+    }
+
+
     @Test
     public void add() throws Exception {
+        Army army = new Army();
+        army.setCount(23);
+        armyService.add(army);
+    }
+
+    @Test
+    public void update() throws Exception {
 
     }
 
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+
 }
+
